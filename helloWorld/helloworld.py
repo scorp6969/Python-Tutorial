80fc108ac4585aeff858fcaa7979b8437cd0d558
scorp6969 <scorpdp6969@gmail.com>
2021-05-01 11:47:40 +0545

    print statement and comment types

---
+++ helloWorld/helloworld.py
@@ -0,0 +1,23 @@
+# single line comment
+# print statement is used for displaying output.
+
+# mutiline comment
+"""
+This 
+is 
+multiple 
+line
+comment.
+"""
+
+#print statement
+print('Hello world!')
+print("This is my first programming langauge.")
+print('')
+print("""
+this
+is
+multiple
+line.
+""")
+print('Python is a programming language.')
