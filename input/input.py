08dec6f58739f7fc7eafb45901178c8c8de0e91a
scorp6969 <scorpdp6969@gmail.com>
2021-05-01 12:55:12 +0545

    input in different datatypes

---
+++ input/input.py
@@ -0,0 +1,8 @@
+name = input('What is your name?: ')
+print('hello ' + name)
+
+age = int(input('What is your age?: '))
+print('You are ' + str(age) + ' years old')
+
+height = float(input('What is your height in cm?: '))
+print('You are ' + str(height) + 'cm tall')
