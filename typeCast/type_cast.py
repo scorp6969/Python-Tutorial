b750e4e19a256734ba3c6acc210bdebfb884d927
scorp6969 <scorpdp6969@gmail.com>
2021-05-01 12:45:55 +0545

    typecast example

---
+++ typeCast/type_cast.py
@@ -0,0 +1,34 @@
+# type casting - convert the datatype of a value to another datatype
+
+# any to string
+a = 1
+b = 2.0
+c = '3'
+
+x = str(a)
+y = str(b)
+z = str(c)
+
+print(x)
+print(y)
+print(z*4)
+print(x + ', ' + y + ', ' + z)
+
+print('')
+# any to int
+b = int(b)
+c = int(c)
+
+print(a)
+print(b)
+print(c)
+
+print('')
+# any to float
+a = float(a)
+b = float(b)
+c = float(c)
+
+print(a)
+print(b)
+print(c)
