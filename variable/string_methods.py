a756f83737f05cfe9e9f64490a83abf86160f675
scorp6969 <scorpdp6969@gmail.com>
2021-05-01 12:04:40 +0545

    string-methods example

---
+++ variable/string_methods.py
@@ -0,0 +1,30 @@
+name = 'rambo rocky'
+n = 'ramborocky'
+name2 = 'RAMBO ROCKY'
+
+# find length of string
+print(len(name))
+
+# find any character in string
+print(name.find('m'))
+
+# capitalize the string
+print(name.capitalize())
+
+# make all character uppercase
+print(name.upper())
+
+# make all character lowercase
+print(name2.lower())
+
+# check whether string contains digit or not
+print(name.isdigit())
+
+# check whether string contains alphabet or not
+print(n.isalpha())
+
+# count any character in string
+print(name.count('o'))
+
+# replace any character with another character
+print(name.replace('o', 'i'))
